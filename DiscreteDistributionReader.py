be7957d1a3255de70f932c958acfc92ccbcb80cb
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2020-02-10 15:20:33 +0100

    Bugfixing

--- DiscreteDistributionReader.py
+++ DiscreteDistributionReader.py
@@ -3,6 +3,8 @@
     task
 """
 
+# TODO: Check y values of discrete points; R.n. the need to get transformed by (1-...)
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,29 +19,26 @@ class DiscreteDistributionReader:
         self.ys = None
         self.xs = None
         # Read in image file at path
-        img = plt.imread(self.path)
-        plt.figure()
-        plt.imshow(img)
-        plt.show()
-        print("image shape before ", img.shape)
-        # Transform the image into grayscale
-        img = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
-        print("image shape after ", img.shape)
-        self.img_shape = np.shape(img)
-        self.img = img
+        self.img = plt.imread(self.path)
+        # TODO: Transform the image into grayscale
+        # self.img = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
+        self.img_shape = np.shape(self.img)
         self.discretize()
 
     # Return discrete values per point from image
     def discretize(self):
-        # TODO: Cut offsets from image
-
-        threshold = 0.5*(np.max(self.img) + np.min(self.img))
+        # TODO: Cut offsets from image?
+        # TODO: Better threshold?
+        #threshold = 0.9*(np.max(self.img) - np.min(self.img))
+        #print("Threshold: ", threshold)
+        threshold = 240
         xs = np.arange(0, self.img_shape[1])
         ys = []
         y_scale = self.img_shape[1]
         for x in xs:
             value = -1
             for y in np.arange(self.img_shape[0]-1, 0, -1):
+                # print(self.img[y, x])
                 if self.img[y, x] <= threshold:
                     value = y/y_scale
                     break
@@ -54,8 +53,6 @@ class DiscreteDistributionReader:
         self.ys = ys
 
         # Indices of discrete points
-        #TODO: Better step size ? RN we use the middle of the point "intervalls" for every discrete x value
-        #self.indices = [int((i+0.5)*len(xs)/(self.points_per_task+1)) for i in range(0, self.points_per_task+1)]
         self.indices = np.arange(0, len(xs) + len(xs)/self.points_per_task, len(xs)/self.points_per_task, dtype=int)
         self.discrete_values = ys[self.indices]
 
@@ -68,15 +65,15 @@ class DiscreteDistributionReader:
         f.suptitle(self.path)
 
         ax1.set_title("Original image")
-        ax1.imshow(self.img, cmap="gray", origin='lower')
+        ax1.imshow(self.img, cmap="gray", origin='upper')
 
+        # TODO: y-Axis should show 0.0 - 1.0 instead of 0.0 - max
         # TODO: x-Axis should show the discrete points rather than the pixel values
-        #plt.scatter(np.arange(0, self.points_per_task+1), self.discrete_values)
-        ax2.scatter(self.xs, self.ys, color='blue', alpha=0.1)
-        ax2.scatter(self.indices, self.discrete_values, color='red', s=120, alpha=1)
+        ax2.scatter(self.xs, 1-self.ys, color='blue', alpha=0.1)
+        ax2.scatter(self.indices, 1-self.discrete_values, color='red', s=120, alpha=1)
         for i, txt in enumerate(np.arange(0, self.points_per_task+1)):
             # TODO: Better alignment for annotations
-            ax2.annotate(txt, (self.indices[i], self.discrete_values[i]))
+            ax2.annotate(txt, (self.indices[i], 1-self.discrete_values[i]))
         ax2.set_ylabel("Confidence")
         ax2.set_xlabel("Reached points")
         ax2.set_title("Discrete values")
@@ -88,7 +85,7 @@ class DiscreteDistributionReader:
     def brier_score(self, points):
         ppt = np.zeros(shape=(self.points_per_task+1, 1))
         ppt[points] = 1
-        bs = np.mean([(self.discrete_values[i] - ppt[i])**2 for i in np.arange(0, len(ppt))])
-        print("Brier-Score of '{}' for {} points is {}.".format(self.path, points, bs))
+        bs = np.mean([((1-self.discrete_values[i]) - ppt[i])**2 for i in np.arange(0, len(ppt))])
+        # print("Brier-Score of '{}' for {} points is {}.".format(self.path, points, bs))
         return bs
 
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # Main Python File
 from DiscreteDistributionReader import DiscreteDistributionReader
 
-dr = DiscreteDistributionReader(path="curve.png", points=5)
-dr2 = DiscreteDistributionReader(path="curve2.jpg", points=5)
-dr3 = DiscreteDistributionReader(path="curve3.png", points=5)
-dr4 = DiscreteDistributionReader(path="curve4.png", points=5)
-dr5 = DiscreteDistributionReader(path="pdf_3.jpg", points=5)
+# dr = DiscreteDistributionReader(path="curve.png", points=5)
+dr1 = DiscreteDistributionReader(path="pdf_1.jpg", points=5)
+dr2 = DiscreteDistributionReader(path="pdf_2.jpg", points=5)
+dr3 = DiscreteDistributionReader(path="pdf_3.jpg", points=5)
 
-dr.plot()
-dr.brier_score(3)
+dr1.plot()
+dr2.plot()
+dr3.plot()
+
+scores1 = [round(dr1.brier_score(i), 2) for i in range(0, 6)]
+scores2 = [round(dr2.brier_score(i), 2) for i in range(0, 6)]
+scores3 = [round(dr3.brier_score(i), 2) for i in range(0, 6)]
+
+print("Brier Scores for Task 1 are: ", scores1)
+print("Brier Scores for Task 2 are: ", scores2)
+print("Brier Scores for Task 3 are: ", scores3)
