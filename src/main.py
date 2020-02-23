d51abf708fa06bb21811ca4238090e01e87cdcee
maxalex <gasolinchen@online.de>
2020-02-23 21:36:46 +0100

    pipeline verändert

--- src/main.py
+++ src/main.py
@@ -1,14 +1,34 @@
 # Main Python File
-from src.discrete_distribution_reader import DiscreteDistributionReader
+import os
+
+from src.discrete_distribution_reader import DiscreteDistributionReader as DDR
+from src.read_pdfs import extract_pdfs
+
+# --------------- Read PDFs --------------- #
+
+subject_code = f'AAAA'
+folder_path = f'assets/subjects/subject_{subject_code}'
+
+image_array = [f'{folder_path}/raw/subject_AAAA_p1.jpg',
+               f'{folder_path}/raw/subject_AAAA_p2.jpg',
+               f'{folder_path}/raw/subject_AAAA_p3.jpg']
+
+extract_pdfs(image_path_array=image_array, dst_folder=f'{folder_path}/pdfs/')
+
+print(os.getcwd())
+
+# --------------- Simulate Distribution --------------- #
 
 # dr = DiscreteDistributionReader(path="curve.png", points=5)
-dr1 = DiscreteDistributionReader(path="../assets/PDFs/pdf_1.jpg", points=5)
-dr2 = DiscreteDistributionReader(path="../assets/PDFs/pdf_2.jpg", points=5)
-dr3 = DiscreteDistributionReader(path="../assets/PDFs/pdf_3.jpg", points=5)
+dr1 = DDR(path=f'{folder_path}/pdfs/pdf_task_1.jpg', points=5)
+dr2 = DDR(path=f'{folder_path}/pdfs/pdf_task_2.jpg', points=5)
+dr3 = DDR(path=f'{folder_path}/pdfs/pdf_task_3.jpg', points=5)
 
 dr1.plot()
 dr2.plot()
 dr3.plot()
+
+# --------------- Calculate Brier Score --------------- #
 
 scores1 = [round(dr1.brier_score(i), 2) for i in range(0, 6)]
 scores2 = [round(dr2.brier_score(i), 2) for i in range(0, 6)]
--- src/read_pdfs.py
+++ src/read_pdfs.py
@@ -3,12 +3,6 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 
-
-subject_code = f'AAAA'
-folder_path = f'assets/subjects/subject_{subject_code}/'
-
-
-
 def sort_contours(cnts, method="left-to-right"):
     """
     From the internet.
@@ -39,108 +33,107 @@ def sort_contours(cnts, method="left-to-right"):
     return (cnts, boundingBoxes)
 
 
-def extract_pdfs(image_array, dst_folder):
+def extract_pdfs(image_path_array, dst_folder, debugging=False):
     """
     Mainly from the internet.
     https://medium.com/coinmonks/a-box-detection-algorithm-
     for-any-image-containing-boxes-756c15d7ed26
     """
-    # Read the image
-    img = cv2.imread(f'{folder_path}/subject_{subject_code}/raw.jpg', 0)
 
-    # Thresholding the image
-    (thresh, img_bin) = cv2.threshold(img, 128, 255,
-                                      cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    # Invert the image
-    img_bin = 255 - img_bin
-    cv2.imwrite(f'{folder_path}/{subject_code}_binary.jpg', img_bin)
+    pdf_task_counter = 0
 
-    # -------------------------- KERNEL ------------------------------------- #
+    for im_num, im_path in enumerate(image_path_array):
 
-    # Defining a kernel length
-    kernel_length = np.array(img).shape[1] // 80
+        # Read the image in grayscale (0 == grayscale)
+        img = cv2.imread(im_path, 0)
 
-    # A verticle kernel of (1 X kernel_length)
-    # which will detect all the verticle lines from the image.
-    verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
+        # Thresholding the image
+        (thresh, img_bin) = cv2.threshold(img, 128, 255,
+                                          cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        # Invert the image
+        img_bin = 255 - img_bin
 
-    # A horizontal kernel of (kernel_length X 1),
-    # which will help to detect all the horizontal line from the image.
-    hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
+        if debugging:
+            cv2.imwrite(f'{dst_folder}debug_im{im_num}_binary.jpg', img_bin)
 
-    # A kernel of (3 X 3) ones.
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        # ------------------------ KERNEL ----------------------------------- #
 
-    # ------------------- MORPHOLOGICAL OPERATIONS -------------------------- #
+        # Defining a kernel length
+        kernel_length = np.array(img).shape[1] // 80
 
-    # Morphological operation to detect verticle lines from an image
-    # Iterations determines (somehow) how long edges can be to be detected
-    # I put from 3 to 2 to detect the small squares with the digits
-    img_temp1 = cv2.erode(img_bin, verticle_kernel, iterations=3)
-    verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
-    cv2.imwrite("assets/PDFs/verticle_lines.jpg", verticle_lines_img)
+        # A verticle kernel of (1 X kernel_length)
+        # which will detect all the verticle lines from the image.
+        verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
 
-    # Morphological operation to detect horizontal lines from an image
-    img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
-    horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
-    cv2.imwrite("assets/PDFs/horizontal_lines.jpg", horizontal_lines_img)
+        # A horizontal kernel of (kernel_length X 1),
+        # which will help to detect all the horizontal line from the image.
+        hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
 
-    # ------------------------ COMBINING LINES ------------------------------ #
+        # A kernel of (3 X 3) ones.
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
-    # Weighting parameters, this will decide the quantity
-    # of an image to be added to make a new image.
-    alpha = 0.5
-    beta = 1.0 - alpha
+        # ----------------- MORPHOLOGICAL OPERATIONS ------------------------ #
 
-    # This function helps to add two image with specific weight parameter
-    # to get a third image as summation of two image.
-    img_final_bin = cv2.addWeighted(verticle_lines_img, alpha,
-                                    horizontal_lines_img, beta, 0.0)
-    img_final_bin = cv2.erode(~img_final_bin, kernel, iterations=2)
-    (thresh, img_final_bin) = cv2.threshold(img_final_bin, 128, 255,
-                                            cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        # Morphological operation to detect verticle lines from an image
+        # Iterations determines (somehow) how long edges can be to be detected
+        # I put from 3 to 2 to detect the small squares with the digits
+        img_temp1 = cv2.erode(img_bin, verticle_kernel, iterations=3)
+        verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
 
-    # FOR DEBUGGING
-    # Enable this line to see verticle and horizontal
-    # lines in the image which is used to find boxes
-    cv2.imwrite("assets/PDFs/img_final_bin.jpg", img_final_bin)
+        # Morphological operation to detect horizontal lines from an image
+        img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
+        horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
+        # ---------------------- COMBINING LINES ---------------------------- #
 
-    # ------------------- FIND AND SORT CONTOURS ---------------------------- #
+        # Weighting parameters, this will decide the quantity
+        # of an image to be added to make a new image.
+        alpha = 0.5
+        beta = 1.0 - alpha
 
-    # Find contours for image, which will detect all the boxes
-    contours, hierarchy = cv2.findContours(
-        img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # This function helps to add two image with specific weight parameter
+        # to get a third image as summation of two image.
+        img_final_bin = cv2.addWeighted(verticle_lines_img, alpha,
+                                        horizontal_lines_img, beta, 0.0)
+        img_final_bin = cv2.erode(~img_final_bin, kernel, iterations=2)
+        (thresh, img_final_bin) = cv2.threshold(img_final_bin, 128, 255,
+                                                cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    # Sort all the contours by top to bottom.
-    (contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
+        # FOR DEBUGGING
+        # Enable this line to see verticle and horizontal
+        # lines in the image which is used to find boxes
+        if debugging:
+            cv2.imwrite(f'{dst_folder}debug_im{im_num}_'
+                        f'lines.jpg', img_final_bin)
 
-    # ------------------- ITERATE AND CUT -------------------------- #
+        # ----------------- FIND AND SORT CONTOURS -------------------------- #
 
-    # TODO: Find out, why it always finds two contours
-    # Because the algorithm always finds 2 times the same contour,
-    # where the first one is slightly to big, we skip always the first
-    skip_matching_pdf = True
+        # Find contours for image, which will detect all the boxes
+        contours, hierarchy = cv2.findContours(
+            img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    num_of_pdf = 0
-    num_of_digit = 0
-    for c in contours:
-        # Returns the location and width,height for every contour
-        x, y, w, h = cv2.boundingRect(c)
+        # Sort all the contours by top to bottom.
+        (contours, boundingBoxes) = sort_contours(
+            contours, method="top-to-bottom")
 
-        # EXTRACT PDFs
-        # If width and height is geater than 80 pixel
-        # and it is approximately square,
-        # we save the contour.
-        if 1.05 * h > w > 80 and 80 < h < 1.05 * w:
-            if not skip_matching_pdf:
-                num_of_pdf += 1
-                new_img = img[y:y + h, x:x + w]
-                cv2.imwrite(f'assets/PDFs/pdf_{num_of_pdf}.jpg', new_img)
-            skip_matching_pdf = not skip_matching_pdf
+        # ------------------- ITERATE AND CUT -------------------------- #
 
+        # TODO: Find out, why it always finds two contours
+        # Because the algorithm always finds 2 times the same contour,
+        # where the first one is slightly to big, we skip always the first
+        skip_matching_pdf = True
 
-image_array = [folder_path + f'raw/subject_AAAA_p1.jpg',
-               folder_path + f'raw/subject_AAAA_p2.jpg',
-               folder_path + f'raw/subject_AAAA_p3.jpg']
+        for c in contours:
+            # Returns the location and width,height for every contour
+            x, y, w, h = cv2.boundingRect(c)
 
-extract_pdfs()
+            # EXTRACT PDFs
+            # If width and height is geater than 80 pixel
+            # and it is approximately square,
+            # we save the contour.
+            if 1.05 * h > w > 80 and 80 < h < 1.05 * w:
+                if not skip_matching_pdf:
+                    pdf_task_counter += 1
+                    new_img = img[y:y + h, x:x + w]
+                    cv2.imwrite(f'{dst_folder}'
+                                f'pdf_task_{pdf_task_counter}.jpg', new_img)
+                skip_matching_pdf = not skip_matching_pdf
