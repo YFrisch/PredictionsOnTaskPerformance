2878dcf97e1a4b477738a629ccfaee9d05aa9054
maxalex <gasolinchen@online.de>
2020-03-08 16:40:51 +0100

    Documentation of utility finished.

--- src/utils.py
+++ src/utils.py
@@ -1,67 +1,41 @@
+"""
+    This file contains diverse small utility functions.
+"""
+
 import os
 import pdf2image
+
+__author__ = 'Maximilian A. Gehrke'
+__date__ = '08-03-2020'
 
 
 def set_working_directory():
     """
-        TODO: Check
-        The working directory is dependent on the configuration of the 
-        python IDE. Here we make sure that the current working directory is set to
-        the 'PredictionsOnTaskPerformance' folder and not the 'src' folder.
+    Setting the working directory to the 'PredictionsOnTaskPerformance'
+    folder, if it is currently in the 'src' folder. This step is
+    necessary as the working directory is dependent on the configuration
+    of the Python IDE.
     """
     current_folder = os.path.basename(os.getcwd())
     if current_folder == f'src':
         os.chdir(f'..')
 
 
-def create_folder(folder_path):
-    """
-
-    :param folder_path:
-    :return:
-    """
-    if not os.path.exists(folder_path):
-        os.mkdir(folder_path)
-    return True
-
-
-def create_analysis_directories(subjects, subjects_folder_path):
-    """
-    TODO
-    :param subjects:
-    :param subjects_folder_path:
-    :return:
-    """
-    for subject in subjects:
-        analysis_path = f'{subjects_folder_path}subject_{subject}/analysis/'
-        create_folder(analysis_path)
-    return True
-
-
-def create_pdf_directories(subjects, subjects_folder_path):
-    """
-    TODO Path where we save the probability density functions
-    :param subjects:
-    :param subjects_folder_path:
-    :return:
-    """
-    for subject in subjects:
-        pdf_path = f'{subjects_folder_path}subject_{subject}/pdfs/'
-        create_folder(pdf_path)
-    return True
-
-
 def extract_subject_codes_from_folders(subjects_folder_path):
     """
-    TODO
-    :param subjects_folder_path:
-    :return:
+    Extract the subject codes by taking all the folder names which start with
+    'subject_' at the given location and extracting the subject codes from
+    them. Our naming convention for subjects is 'subject_' + 'SUBJECTCODE'.
+
+    :param subjects_folder_path: String; path to the subject folders
+    :return: String array containing the subject codes.
     """
-    # Getting all the subdirectory names in the subjects folder
+    # Getting all subdirectories of the subjects folder
     subject_dirs_ = os.listdir(subjects_folder_path)
 
-    # Delete folders that do not start with "subject"
+    # Delete folders that do not start with 'subject_'
     # (e.g. Apples hidden .DS_Store)
+    # and extract the subject code from the folder name.
     subjects = []
     for subject_dir in subject_dirs_:
         if subject_dir.startswith("subject_"):
@@ -70,37 +44,86 @@ def extract_subject_codes_from_folders(subjects_folder_path):
     return subjects
 
 
-def convert_pdf_to_jpg(subjects, subjects_folder_path, file_suffixes):
+def create_folder(folder_path):
     """
+    Create a folder at the given path if it does not already exist.
+    :param folder_path: The path of the desired folder
+    """
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
 
-    :param subjects:
-    :param subjects_folder_path:
-    :param file_suffixes:
-    :return:
+
+def create_analysis_directories(subjects, subjects_folder_path):
+    """
+    Create a folder named analysis for each subject.
+        (This function is hard coded).
+    :param subjects: String array; all subject codes
+    :param subjects_folder_path: String; the path to the subject's folders.
     """
     for subject in subjects:
+        analysis_path = f'{subjects_folder_path}subject_{subject}/analysis/'
+        create_folder(analysis_path)
 
-        # Create array with a seperate string for each file that we
-        # want to read for this subject.
+
+def create_pdf_directories(subjects, subjects_folder_path):
+    """
+    Create a folder named pdf for each subject (hard coded), where we will
+    save all the extracted probability density functions.
+    :param subjects: String array; all subject codes
+    :param subjects_folder_path: String; the path to the subject's folders.
+    """
+    for subject in subjects:
+        pdf_path = f'{subjects_folder_path}subject_{subject}/pdfs/'
+        create_folder(pdf_path)
+
+
+def convert_pdf_to_jpg(subjects, subjects_folder_path, file_suffixes):
+    """
+    Convert PDF pages to JPEG images. The paths are hard coded for this
+    experiment. This function iterates over all subject folders and checks
+    whether JPEG's of the questionnaire already exist in the 'raw' folder.
+    If so, the function does nothing.
+
+    Otherwise the function checks if a PDF is available in the 'raw' folder
+    with the name 'subject_SUBJECTCODE.pdf'. If so, this PDF is read, the
+    first page is discarded and the remaining pages are converted into JPEG's.
+
+    If there is no PDF called 'subject_SUBJECTCODE.pdf' in 'raw', the function
+    expects separate PDF files with one page only, that follow our naming
+    convention.
+
+    NOTE: This function uses the pdf2image library which might need the
+    'poppler' package to be installed.
+
+
+    :param subjects: String array of subject codes.
+    :param subjects_folder_path: String; path to the subjects folder
+    :param file_suffixes: String array with the file endings of the
+        PDF / JPEG's we want to analyse.
+    """
+    for subject in subjects:
+        # Create an array with a separate string for each
+        # file that we want to read for this subject.
         images_array = []
         for fs in file_suffixes:
             images_array.append(f'{subjects_folder_path}'
                                 f'subject_{subject}/raw/subject_{subject}{fs}')
 
-        # First option: one pdf of the whole experiment
-        # 1. page: explanation (incl. subject code), 2. - 4. page: experiment
+        # One PDF for the complete questionnaire
+        # First page: explanation (incl. subject code)
+        # Second to fourth page: experiment
         multiple_pdf_path = f'{subjects_folder_path}subject_{subject}' \
                             f'/raw/subject_{subject}.pdf'
         if os.path.exists(multiple_pdf_path):
             files = pdf2image.convert_from_path(multiple_pdf_path)
-            # skip first page (only explanation)
+            # Skip first page
             for i in range(1, len(files)):
                 file = files[i]
                 fs = file_suffixes[i-1]
                 file.save(f'{multiple_pdf_path[:-4]}{fs}', f'jpeg')
+
+        # Convert single page PDFs into JPEGs
         else:
-            # Convert pdf to jpg if it is not already available in jpg
-            # Note: The "poppler" package needs to be installed
             for img_path in images_array:
                 if not os.path.exists(img_path):
                     files = pdf2image.convert_from_path(f'{img_path[:-3]}pdf')
