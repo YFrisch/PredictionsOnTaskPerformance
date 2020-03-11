f1967fe2aa9c0a9521b4992eaa0f364b9039d8fc
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2020-03-11 15:17:42 +0100

    Calibration evaluation update

--- src/calibration_evaluation.py
+++ src/calibration_evaluation.py
@@ -63,110 +63,6 @@ print(f'Done!')
 
 # --------------- EVALUATE DATA --------------- #
 def calibration(subject_code):
-    """ The calibration() function creates and saves a calibration curve for the given subject.
-
-    The average probability per task is compared with the overall confidence assigned by the subject.
-    #TODO: Average across subjects
-
-    :param subject_code:
-    :return:
-    """
-    # Read in data
-    ts = np.array(list(subject_task_scores.get(subject_code).values())).T.squeeze()
-    prob_matrix = np.empty((1, 6))
-    prob_dict = subject_probs.get(subject_code)
-    for i in range(0, 9):
-        task_prob = np.array(prob_dict.get(i)).reshape((1, -1))
-        prob_matrix = np.concatenate((prob_matrix, task_prob), axis=0)
-    overall_probs = np.copy(prob_matrix.T[:, -1])
-
-    received_points_percentage = np.zeros(shape=(6, 1))
-    for i in range(0, len(ts)):
-        received_points_percentage[ts[i]] += 1
-    received_points_percentage = (received_points_percentage/len(ts)).T.squeeze()
-
-    plt.figure(figsize=(15, 5))
-    plt.title(f'Overall confidence of {subject_code}')
-    for i in range(0, len(overall_probs)):
-        op_i = overall_probs[i]
-        rpp_i = received_points_percentage[i]
-        plt.scatter(i, op_i, color='orange', s=50.0, alpha=1.0, label='Confidence')
-        plt.scatter(i, rpp_i, color='black', s=50.0, alpha=1.0, label='Actual')
-        if rpp_i - op_i < 0:
-            plt.plot([i, i], [rpp_i, op_i], 'k-', color='red', label='Overconfidence')
-        elif rpp_i - op_i > 0:
-            plt.plot([i, i], [rpp_i, op_i], 'k-', color='green', label='Underconfidence')
-
-    handles, labels = plt.gca().get_legend_handles_labels()
-    by_label = OrderedDict(zip(labels, handles))
-    plt.legend(by_label.values(), by_label.keys())
-    plt.plot(range(0, len(overall_probs)), overall_probs, color='orange')
-    plt.plot(range(0, len(received_points_percentage)), received_points_percentage, color='black')
-    plt.fill_between(range(0, len(received_points_percentage)), overall_probs, received_points_percentage,
-                     where=overall_probs >= received_points_percentage, facecolor='red', alpha=0.7, interpolate=True)
-    plt.fill_between(range(0, len(received_points_percentage)), overall_probs, received_points_percentage,
-                     where=overall_probs <= received_points_percentage, facecolor='green', alpha=0.7, interpolate=True)
-    plt.xlabel('Discrete Points')
-    plt.ylabel('Probability')
-
-
-def mod_calibration(subject_codes):
-    """ The calibration() function creates and saves a calibration curve for the given subject.
-
-    The average probability per task is compared with the overall confidence assigned by the subject.
-    #TODO: Average across subjects
-
-    :param subject_code:
-    :return:
-    """
-    # Read in data
-    ts = defaultdict(np.ndarray)
-    ops = defaultdict(np.ndarray)
-    for s in range(0, len(subject_codes)):
-        ts[s] = np.array(list(subject_task_scores.get(s).values())).T.squeeze()
-        prob_matrix = np.empty((1, 6))
-        prob_dict = subject_probs.get(s)
-        for i in range(0, 9):
-            task_prob = np.array(prob_dict.get(i)).reshape((1, -1))
-            prob_matrix = np.concatenate((prob_matrix, task_prob), axis=0)
-        overall_probs = np.copy(prob_matrix.T[:, -1])
-        ops[s] = overall_probs
-
-    received_points_percentage = np.zeros(shape=(6, len(subject_codes)))
-    for s in range(0, len(subject_codes)):
-        for i in range(0, len(ts)):
-            received_points_percentage[ts[i], s] += 1
-        received_points_percentage[:, s] = (received_points_percentage[:, s]/len(ts[s])).T.squeeze()
-    averaged_rpp = np.mean(received_points_percentage, axis=1)
-    averaged_op = np.mean(ops, axis=1)
-    print(f"a_rpp: {averaged_rpp.shape} a_op: {averaged_op.shape}")
-
-    plt.figure(figsize=(15, 5))
-    plt.title(f'Overall average calibration')
-    for i in range(0, len(averaged_op)):
-        op_i = averaged_op[i]
-        rpp_i = averaged_rpp[i]
-        plt.scatter(i, op_i, color='orange', s=50.0, alpha=1.0, label='Confidence')
-        plt.scatter(i, rpp_i, color='black', s=50.0, alpha=1.0, label='Actual')
-        if rpp_i - op_i < 0:
-            plt.plot([i, i], [rpp_i, op_i], 'k-', color='red', label='Overconfidence')
-        elif rpp_i - op_i > 0:
-            plt.plot([i, i], [rpp_i, op_i], 'k-', color='green', label='Underconfidence')
-
-    handles, labels = plt.gca().get_legend_handles_labels()
-    by_label = OrderedDict(zip(labels, handles))
-    plt.legend(by_label.values(), by_label.keys())
-    plt.plot(range(0, len(averaged_op)), averaged_op, color='orange')
-    plt.plot(range(0, len(averaged_rpp)), received_points_percentage, color='black')
-    plt.fill_between(range(0, len(received_points_percentage)), averaged_op, averaged_rpp,
-                     where=averaged_op >= averaged_rpp, facecolor='red', alpha=0.7, interpolate=True)
-    plt.fill_between(range(0, len(averaged_rpp)), averaged_op, averaged_rpp,
-                     where=averaged_op <= averaged_rpp, facecolor='green', alpha=0.7, interpolate=True)
-    plt.xlabel('Discrete Points')
-    plt.ylabel('Probability')
-
-
-def calibration2(subject_code):
     # Gather data
     ts = np.array(list(subject_task_scores.get(subject_code).values())).T.squeeze().reshape(-1, 1)
     prob_matrix = np.empty((1, 6))
@@ -185,30 +81,31 @@ def calibration2(subject_code):
     for i in range(0, len(ts)):
         # Todo: Make max task score modular
         max_score = 5
-        plt.scatter(subject_predictions[i], ts[i]/max_score, s=50, label=f'Task {i+1}')
-        P = np.array([subject_predictions[i], ts[i]/max_score]).reshape(2,)
-        L = np.array([1, 1]).reshape(2,)
-        X = L/np.linalg.norm(L)
-        correction = 0.5 * np.array([1, 1]) * np.dot(P, X)
+
+        a = np.array([subject_predictions[i], ts[i]/max_score]).reshape(2,)
+        plt.scatter(a[0], a[1], s=100, label=f'Task {i+1}')
+        b = np.array([0.5, 0.5]).reshape(2,)
+        correction = np.dot(a, b)/np.linalg.norm(b, 2)
+        correction = correction*b
 
         if subject_predictions[i] > ts[i]/max_score:
-            plt.plot([P[0], correction[0]], [P[1], correction[1]],
-                     color='red', label='Overconfidence')
+            plt.plot([a[0], correction[0]], [a[1], correction[1]],
+                     color='red', label='Overconfidence', alpha=0.7)
         else:
-            plt.plot([P[0], correction[0]], [P[1], correction[1]],
-                     color='blue', label='Underconfidence')
+            plt.plot([a[0], correction[0]], [a[1], correction[1]],
+                     color='blue', label='Underconfidence', alpha=0.7)
 
     plt.plot([0, 1], [0, 1], color='black', label='Optimal accuracy', ls='-')
     plt.xlim(left=0, right=1)
     plt.xlabel("Subjects predicted accuracy")
     plt.ylim(bottom=0, top=1)
     plt.ylabel("Subjects actual accuracy")
-    plt.legend()
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = OrderedDict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys())
     plt.show()
     return None
 
 
 # Create plots for every subjects
-calibration2(f'ATDA')
-
-print(f'# Save Plots ... Done!')
+calibration(f'ATDA')
